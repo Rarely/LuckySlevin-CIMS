1c93840ca363f5e4d8c438cc4659924e5c849ed0
Raghev <raghevmenon@gmail.com>
2014-02-17 00:02:59 -0800

    Fixing bad commit
    n/a

--- app/Controller/TrackingsController.php
+++ app/Controller/TrackingsController.php
@@ -6,16 +6,4 @@ class TrackingsController extends AppController {
         $this->set('title_for_layout', 'Currently Tracked Ideas');
         $this->set('trackings', $this->Tracking->find('all'));
     }
-
-    // //public function view($userid = null) {
-    // //    if (!$userid) {
-    //         throw new NotFoundException(__('Invalid User ID'));
-    //     }
-
-    //     $tracking = $this->Tracking->findById($userid);
-    //     if (!$tracking) {
-    //         throw new NotFoundException(__('Invalid User ID'));
-    //     }
-    //     $this->set('tracking', $tracking);
-    // }
 }
