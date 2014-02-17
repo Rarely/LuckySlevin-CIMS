8d4c5511528e6478ee74e08cef29c2381d82c5fd
graemeCorrin <gcorrin_13@hotmail.com>
2014-02-16 16:45:59 -0800

    Validation on User creation.

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,4 +1,35 @@
 <?php
-class User extends AppModel {
+class User extends appModel {
+    public $validate = array(
+        'email' => array(
+            'email' => array(
+                'rule'     => 'email',
+                'required' => true,
+                'message'  => 'Please enter a valid email address'
+            )
+        ),
+        'password' => array(
+            'alphaNumeric' => array(
+                'rule'     => 'alphaNumeric',
+                'required' => true,
+                'message'  => 'Alphanumeric characters only.'
+            ),
+            'between' => array(
+                'rule'    => array('between', 8, 32),
+                'message' => 'Between 8 to 32 characters'
+            )
+        ),
+        'name' => array(
+            'alphaNumeric' => array(
+                'rule'     => 'alphaNumeric',
+                'required' => true,
+                'message'  => 'Alphanumeric characters only.'
+            ),
+            'between' => array(
+                'rule'    => array('between', 1, 64),
+                'message' => 'Between 1 to 64 characters'
+            )
+        ),
+    );
 }
 ?>
---
+++ app/View/Users/add.ctp
@@ -0,0 +1,32 @@
+<!-- File: /app/View/Posts/add.ctp -->
+
+<h1>Add Post</h1>
+<?php
+echo $this->Form->create('User');
+echo $this->Form->input('title');
+echo $this->Form->input('body', array('rows' => '3'));
+echo $this->Form->end('Create User');
+?>
+
+
+<form role="form">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile">
+    <p class="help-block">Example block-level help text here.</p>
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
