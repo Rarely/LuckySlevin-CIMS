cd938347e894f9452c80d46920468fc333d79182
Jonathan Clark <JonathanClark11@gmail.com>
2014-02-17 23:29:30 -0800

    Tracking Associations + Bug Fix
    fixed associations with tracking so now the page renders and the user data + idea data is there for use.

--- app/Model/Tracking.php
+++ app/Model/Tracking.php
@@ -1,4 +1,13 @@
 <?php 
 class Tracking extends AppModel {
-    public $belongsTo = array('User', 'Idea');
+    public $belongsTo = array(
+            'User' => array('User' => 'User'
+                            ,'className' => 'User'
+                            ,'foreignKey' => 'userid'
+            ),
+            'Idea' => array('Idea' => 'Idea'
+                            ,'className' => 'Idea'
+                            ,'foreignKey' => 'userid'
+            )
+        );
 }
