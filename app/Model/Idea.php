2f8e9df6562d81008b506276b39ed494a4280fa5
Cody <codyjrobinson@hotmail.com>
2014-03-01 13:53:02 -0800

    Small Idea formatting
    Reviewed by Peter

--- app/Model/Idea.php
+++ app/Model/Idea.php
@@ -1,12 +1,20 @@
 <?php
 class Idea extends AppModel {
         public $hasMany = array(
-            'Comments' => array('Comments' => 'Comments'
-                                ,'className' => 'Comments'
-                                ,'foreignKey' => 'ideaid'
-                                ,'order' => 'Comments.created ASC'
+            'Comments' => array('Comments' => 'Comments',
+                                'className' => 'Comments',
+                                'foreignKey' => 'ideaid',
+                                'order' => 'Comments.created ASC'
             )
         );
+
+        public $belongsTo = array(
+            'Users' => array('Users' => 'Users',
+                             'className' => 'Users',
+                             'foreignKey' => 'userid'
+            )
+        );
+
 
 		public function getAllIdeas() {
 			return $this->Idea->find('all');
